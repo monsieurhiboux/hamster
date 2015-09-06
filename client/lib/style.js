be01373de44f12713ee7e8700f6e788a01c52e6d
Nicoaedhel <nicoa.cn@gmail.com>
2015-09-06 19:39:50 +0200

    Edit less and add scrollbar header

--- client/lib/style.js
+++ client/lib/style.js
@@ -3,9 +3,10 @@ Template.layout.onRendered(function(){
 
 Template.header.onRendered(function(){
   this.$('.pop').popup();
-  
+  this.$(".hamster-scrollbar").mCustomScrollbar({theme:"minimal-dark"});
 });
 
 Template.note.onRendered(function(){
   this.$('.pop').popup();
+  this.$("textarea").mCustomScrollbar({theme:"minimal-dark"});
 });
