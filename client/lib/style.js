d8a6e6b8fe25841659eb2be56bb6c621d71d4a61
Nicoaedhel <nicoa.cn@gmail.com>
2015-09-10 15:21:33 +0200

    Edit header selection

--- client/lib/style.js
+++ client/lib/style.js
@@ -2,10 +2,9 @@ Template.layout.onRendered(function(){
 });
 
 Template.header.onRendered(function(){
-  this.$('.pop').popup();
   this.$(".hamster-scrollbar").mCustomScrollbar({theme:"minimal-dark"});
+  setTimeout(function(){$(".hamster-scrollbar a").first().click()}, 50); // too bad
 });
 
 Template.note.onRendered(function(){
-  this.$('.pop').popup();
 });
